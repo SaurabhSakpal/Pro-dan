9f0c746cc095f6ceb6fe8be8f94aad8ac3996734
ssscoder <sfstoe@gmail.com>
2012-06-15 16:30:15 +0530

    Final commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,26 +8,53 @@ end
 def login
 	emal = params[:home]['email']
 	passwrd1 = params[:home]['password']
-	if emal != nil && passwrd1 != nil
-		@user = User.find_by_email(emal)
-		passwrd2 = @user.password
-		if @user.status == 'NO'
-			flash[:notice] = "Verify your email first "
-			redirect_to :controller => 'home',:action=>'show'
-		else
-			if (passwrd1 == passwrd2)
-				session[:user_id] = @user.id
-				redirect_to :controller => 'profiles',:action=>'show', :id=>@user.id
-				flash[:notice] = "successfully logged in"
-			else
+	a = emal.split('@').size
+	if a>=2
+		if emal != nil && passwrd1 != nil
+			@user = User.find_by_email(emal)
+			passwrd2 = @user.password
+			if @user.status == 'NO'
+				flash[:notice] = "Verify your email first "
 				redirect_to :controller => 'home',:action=>'show'
-				flash[:notice] = "Invalid user name/password"
+			else
+				if (passwrd1 == passwrd2)
+					session[:user_id] = @user.id
+					redirect_to :controller => 'profiles',:action=>'show', :id=>@user.id
+					flash[:notice] = "successfully logged in using email"
+				else
+					redirect_to :controller => 'home',:action=>'show'
+					flash[:notice] = "Invalid user name/password"
+				end
 			end
+		else
+			reset_session
+			flash[:notice] = "Unable to login please. password/email shouldnt be blank"
+			redirect_to :controller => 'home',:action=>'show'
 		end
 	else
-		reset_session
-		flash[:notice] = "Unable to login please. password/email shouldnt be blank"
-		redirect_to :controller => 'home',:action=>'show'
+		if emal != nil && passwrd1 != nil
+			@userid = Userid.find_by_userid(emal)
+			@user = Userid.find_by_userid(emal).user
+			passwrd2 = @user.password
+			if @userid.status == 'NO'
+				flash[:notice] = "Verify your UNIQUE USER ID first "
+				redirect_to :controller => 'home',:action=>'show'
+			else
+				if (passwrd1 == passwrd2)
+					session[:user_id] = @user.id
+					redirect_to :controller => 'profiles',:action=>'show', :id=>@user.id
+					flash[:notice] = "successfully logged in using email"
+				else
+					redirect_to :controller => 'home',:action=>'show'
+					flash[:notice] = "Invalid UNIQUE USERID/password"
+				end
+			end
+		else
+			reset_session
+			flash[:notice] = "Unable to login please. password/USERID shouldnt be blank"
+			redirect_to :controller => 'home',:action=>'show'
+		end
+	
 	end
 end
 
